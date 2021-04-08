89c3faeebe4433ca4d67909e8c158e9372e0b46c
Jürgen Kober <mindeleven@posteo.de>
2021-04-08 12:03:37 +0200

    Elaborating on the layout of the TimerForm component

--- time-tracking/src/components/TimerForm.js
+++ time-tracking/src/components/TimerForm.js
@@ -1,14 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-function TimerForm() {
+function TimerForm({ id, title, project }) {
   return (
-    <View>
-      <Text>TimerForm</Text>
+    <View style={styles.formContainer}>
+      <View style={styles.attributeContainer}>
+        <Text style={styles.textInputTitle}>Title</Text>
+        <View style={styles.textInputContainer}>
+          <TextInput
+            style={styles.TextInput}
+            underlineColorAndroid="transparent"
+            defaultValue={title}
+          />
+        </View>
+      </View>
+      <View style={styles.attributeContainer}>
+        <Text style={styles.textInputTitle}>Project</Text>
+        <View style={styles.textInputContainer}>
+          <TextInput
+            style={styles.TextInput}
+            underlineColorAndroid="transparent"
+            defaultValue={project}
+          />
+        </View>
+      </View>
+      <View style={styles.buttonGroup}>
+        <Button
+          title="submitText"
+        />
+        <Button
+          title="Cancel"
+        />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  formContainer: {
+    backgroundColor: 'white',
+    borderColor: '#D6D7DA',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 15,
+    margin: 15,
+    marginBottom: 0
+  },
+  attributeContainer: {
+    marginVertical: 8
+  },
+  textInputContainer: {
+    borderColor: '#D6D7DA',
+    borderRadius: 2,
+    borderWidth: 1,
+    marginBottom: 6
+  },
+  textInput: {
+    height: 30,
+    padding: 5,
+    fontSize: 12
+  },
+  textInputTitle: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginBottom: 5
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
 
 export default TimerForm;
