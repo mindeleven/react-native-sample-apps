8bb6f33ea9e19a91cb610d231ed932185dbd6bff
Jürgen Kober <mindeleven@posteo.de>
2021-04-08 17:29:54 +0200

    Elaborating on the layout of the ToggleableTimerForm component

--- time-tracking/src/components/ToggleableTimerForm.js
+++ time-tracking/src/components/ToggleableTimerForm.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import TimerButton from './TimerButton';
+import TimerForm from './TimerForm';
 
-function ToggleableTimerForm() {
+function ToggleableTimerForm({ isOpen }) {
   return (
-    <View>
-      <Text>ToggleableTimerForm</Text>
+    <View style={[styles.container, !isOpen && styles.buttonPadding]}>
+      {isOpen ? (
+        <TimerForm />
+      ) : (
+        <TimerButton title="+" color="black" />
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10
+  },
+  buttonPadding: {
+    paddingHorizontal: 15
+  }
+});
 
 export default ToggleableTimerForm;
