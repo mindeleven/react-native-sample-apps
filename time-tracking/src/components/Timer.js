888c52a938a6524f26d4381253bf1f62475d3d97
Jürgen Kober <mindeleven@posteo.de>
2021-04-10 17:59:54 +0200

    Elaborating on the layout of the Timer component

--- time-tracking/src/components/Timer.js
+++ time-tracking/src/components/Timer.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-function EditableTimer() {
+import { millisecondsToHuman } from '../../utils/TimerUtils';
+import TimerButton from './TimerButton';
+
+function Timer({ title, project, elapsed }) {
+
+  const elapsedString = millisecondsToHuman(elapsed);
+
   return (
-    <View>
-      <Text>EditableTimer</Text>
+    <View style={styles.timerContainer}>
+      <Text style={styles.title}>{title}</Text>
+      <Text>{project}</Text>
+      <Text style={styles.elapsedTime}>{elapsedString}</Text>
+      <View style={styles.buttonGroup}>
+        <TimerButton color="blue" small title="Edit" />
+        <TimerButton color="blue" small title="Remove" />
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  timerContainer: {
+    backgroundColor: 'white',
+    borderColor: '#D6D7DA',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 15,
+    margin: 15,
+    marginBottom: 0
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold'
+  },
+  elapsedTime: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 15
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
 
-export default EditableTimer;
+export default Timer;
